964eeffc8ecd339d16b7d071369edd8b1c3e43ab
SnoeProductions <78712755+Jsnoe26@users.noreply.github.com>
2021-06-10 16:24:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let nums = [3.14, 42, 4811];
+let divided = function(n) {
+  return n/2;
+};
+
+let halved = nums.map(divided);
+
+// TODO: Write a mapping function
+// and pass it to .map()
+
+
+console.log(halved);
